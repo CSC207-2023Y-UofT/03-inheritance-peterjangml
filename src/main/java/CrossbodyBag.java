ccecbfce66087a71b4e2d35e8091e06d9e59a39c
peterjangml <hspeterj@gmail.com>
2023-06-11 16:16:22 -0400

    submission 6 (revert)

--- src/main/java/CrossbodyBag.java
+++ src/main/java/CrossbodyBag.java
@@ -32,8 +32,11 @@
  */
 
 public class CrossbodyBag extends Bag{
+    private int numberOfStraps;
+
     public CrossbodyBag(String color, int capacity, int numberOfStraps) {
         super(color, capacity);
+        this.numberOfStraps = numberOfStraps;
     }
 
     public int getNumberOfStraps() {
