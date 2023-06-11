da773d7bd957ef8e8b07eb80f71e99a65190b252
peterjangml <hspeterj@gmail.com>
2023-06-11 16:13:35 -0400

    submission 5 (removed crossbodybag bug)

--- src/main/java/CrossbodyBag.java
+++ src/main/java/CrossbodyBag.java
@@ -32,11 +32,8 @@
  */
 
 public class CrossbodyBag extends Bag{
-    private int numberOfStraps;
-
     public CrossbodyBag(String color, int capacity, int numberOfStraps) {
         super(color, capacity);
-        this.numberOfStraps = numberOfStraps;
     }
 
     public int getNumberOfStraps() {
