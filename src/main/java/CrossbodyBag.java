b89262b8d3603753f195d90a361492864c506e88
peterjangml <hspeterj@gmail.com>
2023-06-11 16:44:58 -0400

    test (toString)

--- src/main/java/CrossbodyBag.java
+++ src/main/java/CrossbodyBag.java
@@ -50,6 +50,7 @@ public class CrossbodyBag extends Bag {
 
     @Override
     public String toString() {
+        System.out.prinln("Crossbody " + super.toString().replace("Bag", "Bag with " + numberOfStraps + " straps");
         return "Crossbody " + super.toString().replace("Bag", "Bag with " + numberOfStraps + " straps");
     }
 }
